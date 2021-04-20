4792d035b62b567657c262a590ad43da990816b5
Martin Ivanov <43958330+martinrivanov@users.noreply.github.com>
2021-04-20 11:57:33 +0300

    Delete smash-template-opl directory

--- smash-template-opl/assets/websites/The Eating Healthy Experience/script.js
+++
@@ -1,8 +0,0 @@
-let btn = document.getElementById('cta-btn');
-let overlay = document.getElementById('overlay');
-
-if(btn){
-    btn.addEventListener('click', () => {
-        overlay.style.display = 'grid';
-    })
-}
